2d0d25229126382d7e4e8216e6d350fd2b34cf92
Rudolf Jacobs <rudolf@a-i-solutions.co.za>
2023-06-26 09:00:01 +0200

    [gui] added 4th tab [chore] updated gitignore

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 from Libraries.whatsapp_utils import whatsapp_gui
 from Libraries.excel_parser import parse_excel_gui
 from Libraries.export_pdf_pages import export_pdf_gui
+from Libraries.password_remover import remove_pwd_gui
 
 # Create the GUI window
 window = tk.Tk()
@@ -21,6 +22,9 @@ parse_excel_gui(tab_control)
 
 # Tab 3 - Pdf Splitter
 export_pdf_gui(tab_control)
+
+# Tab 4 - Pdf Pwd remover
+remove_pwd_gui(tab_control)
 
 tab_control.pack(expand=1, fill="both")
 window.mainloop()
